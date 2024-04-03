5f13fb0ba798dcc99731f5245b1f49e88f48851d
mak48 <makoveevaanastasija@yandex.ru>
2024-04-04 00:08:20 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -17,14 +20,27 @@ public class Main {
             System.out.println("Пол: женский");
         else
             System.out.println("Пол: мужской");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate startDate = LocalDate.now();
+
         try {
-            LocalDate endDate = LocalDate.parse(date, formatter);
-            Period period = Period.between(endDate,startDate);
-            System.out.println("Возраст: "+period.getYears());
-        }catch (DateTimeParseException e) {
-            System.out.println("Date in wrong format");
+            SimpleDateFormat df = new java.text.SimpleDateFormat("dd.MM.yyyy");
+            df.setLenient(false);
+            df.parse(date);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+            LocalDate startDate = LocalDate.now();
+            try {
+                LocalDate endDate = LocalDate.parse(date, formatter);
+                Period period = Period.between(endDate,startDate);
+                if (period.getYears()%10==0 || period.getYears()%10>4 || period.getYears()%100>10 && period.getYears()%100<15)
+                    System.out.println("Возраст: "+period.getYears()+" лет");
+                else if (period.getYears()%10==1)
+                    System.out.println("Возраст: "+period.getYears()+" год");
+                else
+                    System.out.println("Возраст: "+period.getYears()+" года");
+            }catch (DateTimeParseException e) {
+                System.out.println("Date in wrong format");
+            }
+        } catch (java.text.ParseException e) {
+            System.out.println("ParseException: Unparseable date");
         }
     }
 }
